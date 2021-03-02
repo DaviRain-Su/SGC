f30c8a6f269d186c9190842753a90cbe62844fea
DaviRain-Su <davirain.yin@gamil.com>
2021-03-02 16:28:30 +0800

    update ...

--- pallets/erc20/erc20-extersion/src/lib.rs
+++ pallets/erc20/erc20-extersion/src/lib.rs
@@ -2,20 +2,66 @@
 
 use codec::{Encode, Decode};
 
-use frame_support::debug::{error, native};
+use frame_support::debug::{self, error, native};
 use frame_support::traits::Randomness;
 
 use pallet_contracts::chain_extension::{
 	ChainExtension, Environment, Ext, InitState, RetVal, SysConfig, UncheckedFrom,
 };
-use sp_runtime::DispatchError;
+
+use sp_runtime::{DispatchError, RuntimeDebug};
 
 
-pub trait Config: pallet_contracts::Config {
+pub trait Config: pallet_contracts::Config + pallet_erc20::Config {
 	type Randomness : Randomness<Self::Hash>;
 }
 
-/// contract extension for `FetchRandom`
+#[derive(Clone, Encode, Decode, PartialEq, Eq, RuntimeDebug)]
+pub struct NewInputParam<Balance> {
+	total_supply: Balance,
+}
+
+#[derive(Clone, Encode, Decode, PartialEq, Eq, RuntimeDebug)]
+pub struct BalanceOfInputParam<AccountId> {
+	owner: AccountId,
+}
+
+#[derive(Clone, Encode, Decode, PartialEq, Eq, RuntimeDebug)]
+pub struct AllowanceOfInputParam<AccountId> {
+	owner: AccountId,
+	spender: AccountId,
+}
+
+
+#[derive(Clone, Encode, Decode, PartialEq, Eq, RuntimeDebug)]
+pub struct TransferInputParam<AccountId, Balance> {
+	to: AccountId,
+	value: Balance,
+}
+
+#[derive(Clone, Encode, Decode, PartialEq, Eq, RuntimeDebug)]
+pub struct AllowanceInputParam<AccountId, Balance> {
+	owner: AccountId,
+	spender: AccountId,
+	value: Balance,
+}
+
+
+#[derive(Clone, Encode, Decode, PartialEq, Eq, RuntimeDebug)]
+pub struct TransferFromInputParam<AccountId, Balance> {
+	from: AccountId,
+	to: AccountId,
+	value: Balance,
+}
+
+#[derive(Clone, Encode, Decode, PartialEq, Eq, RuntimeDebug)]
+pub struct TransferHelpInputParam<AccountId, Balance> {
+	from: AccountId,
+	to: AccountId,
+	value: Balance,
+}
+
+
 pub struct FetchRandomExtension;
 
 impl<C: Config> ChainExtension<C> for FetchRandomExtension {
@@ -26,7 +72,8 @@ impl<C: Config> ChainExtension<C> for FetchRandomExtension {
 	{
 
 		match func_id {
-			1101 => {
+			1101 => { // function random_seed()
+				debug::info!("func_id 1101");
 				let mut env = env.buf_in_buf_out();
 				// let random_seed: [u8; 32] = super::RandomnessCollectiveFlip::random_seed().0;
 				let random_slice = <E::T as Config>::Randomness::random_seed().encode();
@@ -38,6 +85,136 @@ impl<C: Config> ChainExtension<C> for FetchRandomExtension {
 				);
 				env.write(&random_slice, false, None)
 					.map_err(|_| DispatchError::Other("ChainExtension failed to call random"))?;
+			}
+			1102 => { // pallet-erc20 function create()
+				debug::info!("func_id create() 1102");
+				let mut env = env.buf_in_buf_out();
+				let caller = env.ext().caller().clone();
+				debug::info!("caller: {:?}", caller);
+
+				let input: NewInputParam<
+					<E::T as pallet_erc20::Config>::Balance
+				> = env.read_as()?;
+				debug::info!("input: {:?}", input);
+
+				pallet_erc20::Module::<E::T>::create(caller, input.total_supply)?;
+			}
+			1103 => { // pallet-erc20 function total_supply
+				debug::info!("func_id total_supply() 1103");
+				let mut env = env.buf_in_buf_out();
+				let caller = env.ext().caller().clone();
+				debug::info!("caller: {:?}", caller);
+
+				let balance: u128 = pallet_erc20::Module::<E::T>::total_supply().into();
+				debug::info!("balance: {:?}", balance);
+
+				let balance_slice = balance.to_be_bytes();
+				debug::info!("balance_silce: {:?}", balance_slice);
+
+				debug::native::trace!(
+					target: "runtime",
+					"[ChainExtension]|call|func_id:{:}",
+					func_id
+				);
+				env.write(&balance_slice, false, None)
+					.map_err(|_| DispatchError::Other("ChainExtension failed to call create collection"))?;
+			}
+			1104 => { // pallet-erc20 function balance_of
+				debug::info!("func_id balance_of 1104");
+				let mut env = env.buf_in_buf_out();
+				let caller = env.ext().caller().clone();
+				debug::info!("caller : {:?}", caller);
+
+				let input: BalanceOfInputParam<
+					<E::T as SysConfig>::AccountId
+				> = env.read_as()?;
+				debug::info!("input: {:?}",input);
+
+				let balance: u128 = pallet_erc20::Module::<E::T>::balance_of(input.owner).into();
+				debug::info!("balance: {:?}", balance);
+
+				let balance_slice = balance.to_be_bytes();
+				debug::info!("balance_slice: {:?}", balance_slice);
+
+				debug::native::trace!(
+					target: "runtime",
+					"[ChainExtension]|call|func_id:{:?}",
+					func_id
+				);
+
+				env.write(&balance_slice, false, None)
+					.map_err(|_| DispatchError::Other("ChainExtension failed to call create collection"))?;
+			}
+			1105 => { // pallet-erc20 function allowance_of
+				debug::info!("func_id allowance_of 1105");
+				let mut env = env.buf_in_buf_out();
+				let caller = env.ext().caller().clone();
+				debug::info!("caller : {:?}", caller);
+
+				let input: AllowanceOfInputParam<
+					<E::T as SysConfig>::AccountId
+				> = env.read_as()?;
+				debug::info!("input: {:?}", input);
+
+				let balance: u128 = pallet_erc20::Module::<E::T>::allowance_of(input.owner, input.spender).into();
+				debug::info!("balance: {:?}", balance);
+
+				let balance_slice = balance.to_be_bytes();
+				debug::info!("balance_slice: {:?}", balance_slice);
+
+				debug::native::trace!(
+					target: "runtime",
+					"[ChainExtension]|call|func_id:{:?}",
+					func_id
+				);
+
+				env.write(&balance_slice, false, None)
+					.map_err(|_| DispatchError::Other("ChainExtension failed to call create collection"))?;
+			}
+
+			1106 => { // pallet-erc20 function transfer_from
+				debug::info!("func_id transfer_from 1106");
+				let mut env = env.buf_in_buf_out();
+				let caller = env.ext().caller().clone();
+				debug::info!("caller: {:?}", caller);
+
+				let input: TransferFromInputParam<
+					<E::T as SysConfig>::AccountId,
+					<E::T as pallet_erc20::Config>::Balance
+				> = env.read_as()?;
+				debug::info!("input: {:?}", input);
+
+				pallet_erc20::Module::<E::T>::do_transfer_from(input.from, input.to, input.value)?;
+			}
+
+			1107 => { // pallet-erc20 function transfer_help
+				debug::info!("func_id transfer_help 1107");
+				let mut env = env.buf_in_buf_out();
+				let caller = env.ext().caller().clone();
+				debug::info!("caller: {:?}", caller);
+
+				let input: TransferHelpInputParam<
+					<E::T as SysConfig>::AccountId,
+					<E::T as pallet_erc20::Config>::Balance
+				> = env.read_as()?;
+				debug::info!("input: {:?}", input);
+
+				pallet_erc20::Module::<E::T>::transfer_help(input.from, input.to, input.value)?;
+			}
+
+			1108 => { // pallet-erc20  function allowance
+				debug::info!("func_id allowance");
+				let mut env = env.buf_in_buf_out();
+				let caller = env.ext().caller().clone();
+				debug::info!("caller: {:?}", caller);
+
+				let input: AllowanceInputParam<
+					<E::T as SysConfig>::AccountId,
+					<E::T as pallet_erc20::Config>::Balance,
+				> = env.read_as()?;
+				debug::info!("input: {:?}", input);
+
+				pallet_erc20::Module::<E::T>::do_allowance(input.owner, input.spender, input.value)?;
 			}
 
 			_ => {
--- pallets/erc20/src/lib.rs
+++ pallets/erc20/src/lib.rs
@@ -2,6 +2,7 @@
 pub use pallet::*;
 
 use frame_support::dispatch;
+use frame_support::debug;
 
 
 #[cfg(test)]
@@ -21,7 +22,7 @@ pub mod pallet {
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
 		type Balance: Parameter + Member + AtLeast32BitUnsigned + Codec + Default + Copy +
-		MaybeSerializeDeserialize + Debug;
+		MaybeSerializeDeserialize + Debug + From<u128> + Into<u128>;
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
 	}
 
@@ -31,17 +32,17 @@ pub mod pallet {
 
 	// 定义总供给量
 	#[pallet::storage]
-	#[pallet::getter(fn total_supply_of)]
+	#[pallet::getter(fn total_supply_by)]
 	pub type TotalSupply<T: Config> = StorageValue<_, T::Balance>;
 
 	// 定义一个账户对应的余额
 	#[pallet::storage]
-	#[pallet::getter(fn balances_of)]
+	#[pallet::getter(fn balances_by)]
 	pub type Balances<T: Config> = StorageMap<_, Blake2_128Concat, T::AccountId, T::Balance, ValueQuery >;
 
 	// 定义A账户可以向B账户转账的金额
 	#[pallet::storage]
-	#[pallet::getter(fn allowance_of)]
+	#[pallet::getter(fn allowance_by)]
 	pub type Allowance<T: Config> = StorageMap<_, Blake2_128Concat, (T::AccountId, T::AccountId), T::Balance>;
 
 	#[pallet::event]
@@ -65,6 +66,7 @@ pub mod pallet {
 
 	#[pallet::call]
 	impl<T:Config> Pallet<T> {
+
 		#[pallet::weight(10_1000 + T::DbWeight::get().writes(1))]
 		pub fn transfer(origin: OriginFor<T>, to: T::AccountId, value: T::Balance) -> DispatchResultWithPostInfo {
 			let who = ensure_signed(origin)?;
@@ -77,16 +79,17 @@ pub mod pallet {
 		#[pallet::weight(10_1000 + T::DbWeight::get().writes(1))]
 		pub fn transfer_from(origin: OriginFor<T>, from: T::AccountId, to: T::AccountId, value: T::Balance) -> DispatchResultWithPostInfo {
 			let _ = ensure_signed(origin)?;
-
-			let from_balance = Balances::<T>::get(&from);
-
-			if from_balance < value {
-				return Err(Error::<T>::InSufficientAllowance)?;
-			}
-
-			Allowance::<T>::insert((from.clone(), to.clone()), from_balance - value);
-
-			Self::transfer_help(from, to, value)?;
+			//
+			// let from_balance = Balances::<T>::get(&from);
+			//
+			// if from_balance < value {
+			// 	return Err(Error::<T>::InSufficientAllowance)?;
+			// }
+			//
+			// Allowance::<T>::insert((from.clone(), to.clone()), from_balance - value);
+			//
+			// Self::transfer_help(from, to, value)?;
+			Self::do_transfer_from(from, to, value)?;
 
 			Ok(().into())
 		}
@@ -95,14 +98,16 @@ pub mod pallet {
 		pub fn allowance(origin: OriginFor<T>, spender: T::AccountId, value: T::Balance) -> DispatchResultWithPostInfo {
 			let who = ensure_signed(origin)?;
 
-			let who_balance = Balances::<T>::get(&who);
-			if who_balance < value {
-				return Err(Error::<T>::InSufficientBalance)?;
-			}
+			// let who_balance = Balances::<T>::get(&who);
+			// if who_balance < value {
+			// 	return Err(Error::<T>::InSufficientBalance)?;
+			// }
+			//
+			// Allowance::<T>::insert((who.clone(), spender.clone()), value);
+			//
+			// Self::deposit_event(Event::Approve(who, spender, value));
 
-			Allowance::<T>::insert((who.clone(), spender.clone()), value);
-
-			Self::deposit_event(Event::Approve(who, spender, value));
+			Self::do_allowance(who, spender, value)?;
 
 			Ok(().into())
 		}
@@ -110,6 +115,46 @@ pub mod pallet {
 }
 
 impl<T: Config> Pallet<T> {
+
+	pub fn create(owner: T::AccountId, total_supply: T::Balance) -> dispatch::DispatchResult {
+
+		//set initial supply
+		TotalSupply::<T>::put(total_supply);
+		// set alias who and total supply
+		Balances::<T>::insert(owner, total_supply);
+
+		Ok(().into())
+	}
+
+	pub fn do_transfer_from(from: T::AccountId, to: T::AccountId, value: T::Balance) -> dispatch::DispatchResult {
+
+		let from_balance = Balances::<T>::get(&from);
+
+		if from_balance < value {
+			return Err(Error::<T>::InSufficientAllowance)?;
+		}
+
+		Allowance::<T>::insert((from.clone(), to.clone()), from_balance - value);
+
+		Self::transfer_help(from, to, value)?;
+
+		Ok(().into())
+	}
+
+	pub fn do_allowance(who: T::AccountId, spender: T::AccountId, value: T::Balance) -> dispatch::DispatchResult {
+
+		let who_balance = Balances::<T>::get(&who);
+		if who_balance < value {
+			return Err(Error::<T>::InSufficientBalance)?;
+		}
+
+		Allowance::<T>::insert((who.clone(), spender.clone()), value);
+
+		Self::deposit_event(Event::Approve(who, spender, value));
+
+		Ok(().into())
+	}
+
 	pub fn transfer_help(from: T::AccountId, to : T::AccountId, value: T::Balance) -> dispatch::DispatchResult {
 		let from_balance = Balances::<T>::get(&from);
 		if from_balance < value {
@@ -123,6 +168,30 @@ impl<T: Config> Pallet<T> {
 		Balances::<T>::insert(to.clone(), to_balance + value);
 
 		Self::deposit_event(Event::Transfer(from, to, value));
+
 		Ok(())
+	}
+
+	pub fn total_supply() -> <T as pallet::Config>::Balance {
+		debug::info!("erc20 total supply of!");
+
+		// Self::total_supply_by()
+		TotalSupply::<T>::get().unwrap()
+	}
+
+	pub fn balance_of(owner: T::AccountId) -> T::Balance {
+		debug::info!("erc20 balance_of!");
+		debug::info!("owner is {:?}", owner);
+
+		// Self::balance_by(owner)
+		Balances::<T>::get(&owner)
+	}
+
+	pub fn allowance_of(owner: T::AccountId, spender: T::AccountId) -> T::Balance {
+		debug::info!("erc20 allowance of!");
+		debug::info!("owner: {:?}, spender: {:?}", owner, spender);
+
+		// Self::alowance_by(owner, spender)
+		Allowance::<T>::get(&(owner, spender)).unwrap()
 	}
 }
